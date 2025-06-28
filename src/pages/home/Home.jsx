81257c44bf1745ef10be60eec107a93d4500dfba
Mxhrshad <mserattalab@gmail.com>
2025-06-28 19:01:11 +0330

    edit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,8 @@
-import ChefsRecommendations from "../../components/chefs-recommendations/Chefsrecommendations";
+import ChefsRecommendations from "../../components/chefs-recommendations/ChefsRecommendations";
 import Footer from "../../components/footer/Footer";
 import HeroSection from "../../components/hero-section/HeroSection";
-import MenuCategories from "../../components/menu-categories/Menucategories";
-import MenuItemsDisplay from "../../components/menu-items-display/Menuitemsdisplay";
+import MenuCategories from "../../components/menu-categories/MenuCategories";
+import MenuItemsDisplay from "../../components/menu-items-display/MenuItemsDisplay";
 import Navbar from "../../components/navbar/Navbar";
 
 export default function Home() {
