2bdd789864128ed5c3be32bdd5d6a7cf173f932c
Mxhrshad <mserattalab@gmail.com>
2025-06-28 18:58:06 +0330

    edit

--- src/pages/Product/ProductPage.jsx
+++ src/pages/Product/ProductPage.jsx
@@ -1,4 +1,4 @@
-import Footer from "../../components/footer/Footer";
+import Footer from '../../components/footer/Footer';
 import Navbar from "../../components/navbar/Navbar";
 import Product from "../../components/product/Product";
 
